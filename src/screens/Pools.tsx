a8f2529aeefb86c013b4f13fd381c960a7691eff
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2022-11-09 00:29:24 -0300

    Criação bolões, listagem de bolões criados

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -1,12 +1,45 @@
-import { VStack, Icon } from "native-base";
-import { Octicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useEffect, useState } from "react";
+import { VStack, Icon, useToast, FlatList } from "native-base";
+import { Octicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+import { api } from "../services/api";
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { Loading } from "../components/Loading";
+import { PoolCard, PoolCardProps } from "../components/PoolCard";
 
 export function Pools() {
-  const {navigate} = useNavigation ();
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardProps[]>([]);
+
+  const { navigate } = useNavigation();
+  const toast = useToast();
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true);
+
+      const response = await api.get("/pools");
+      setPools(response.data.pools);
+    } catch (error) {
+      console.log(error);
+
+      toast.show({
+        title: "Não foi possivel carregar os bolões",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchPools();
+  }, []);
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Meus bolões" />
@@ -19,11 +52,20 @@ export function Pools() {
         pb={4}
         mb={4}
       >
-        <Button title="BUSCAR BOLÃO POR CÓDIGO" 
-            leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}
-            onPress={() => navigate('find')}
+        <Button
+          title="BUSCAR BOLÃO POR CÓDIGO"
+          leftIcon={
+            <Icon as={Octicons} name="search" color="black" size="md" />
+          }
+          onPress={() => navigate("find")}
         />
       </VStack>
+
+      <FlatList
+        data={pools}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <PoolCard data={item} />}
+      />
     </VStack>
   );
 }
